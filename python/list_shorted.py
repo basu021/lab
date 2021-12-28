183d3f1d55835737932b16910f490f1ad8985387
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2021-12-28 16:47:55 +0530

    12.28.21

---
+++ python/list_shorted.py
@@ -0,0 +1,9 @@
+#print list in a shorted way
+
+a = [1, 2, 0, 9, 43, 23, 11]
+
+#short a list
+
+a.sort()
+
+print(a)
---
+++ python/matrix_addition.py
@@ -0,0 +1,8 @@
+#add two 1d matrices
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a)
+print(b)
+for i in range(len(a)):
+    print(a[i] + b[i])
